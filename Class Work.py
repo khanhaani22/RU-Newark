bd5fb909a93a981ef4e005e08fa42fd76e9c9272
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2024-09-26 23:49:27 +0000

    i did it!

---
+++ Class Work.py
@@ -0,0 +1,22 @@
+#SEPTEMBER 26 2024
+#1.1
+day = input("Whats the day today? ")
+if day == ["Monday","Tuesday","Wednesday", "Friday"]:
+    print("Time to study!")
+elif day == "Thursday":
+    print("Class")
+elif day == ["Saturday","Sunday"]:
+    print("Have fun!")
+
+#1.2
+age = int(input("How old are you? (Input number)\n"))
+if age == 1:
+    print("You are an infant.")
+elif age > 1 and age < 13:
+    print("You are a child.")
+elif age > 12 and age < 20:
+    print("You are a teenager.")
+elif age > 20:
+    print("You are an adult.")
+else:
+    print("You are not born.")
