f824447836cd38fd4b57a6fec203488bdf1229c4
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2024-10-03 22:11:06 +0000

    yes

--- Classwork/September 26, 2024.py
+++ Classwork/September 26, 2024.py
@@ -1,6 +1,5 @@
-#SEPTEMBER 26 2024
 #1.1
-day = input("Whats the day today? ")
+day = input("Whats the day today? (Type out day of the week)\n")
 if day == ["Monday","Tuesday","Wednesday", "Friday"]:
     print("Time to study!")
 elif day == "Thursday":
@@ -10,13 +9,13 @@ elif day == ["Saturday","Sunday"]:
 
 #1.2
 age = int(input("How old are you? (Input number)\n"))
-if age == 1:
+if (age == 1):
     print("You are an infant.")
-elif age > 1 and age < 13:
+elif (age > 1 and age < 13):
     print("You are a child.")
-elif age > 12 and age < 20:
+elif (age > 12 and age < 21):
     print("You are a teenager.")
-elif age > 20:
+elif (age > 20):
     print("You are an adult.")
 else:
     print("You are not born.")
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 #TEST FIELD tbh not gonna lie
+city_one = input("Enter City One: ")
+
+city_two = input("Enter City Two: ")
+
+print(sort(city_one, city_two))
