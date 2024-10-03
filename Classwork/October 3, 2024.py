c5152e9afa63cd1a4711fca689bb8b25bdcf8848
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2024-10-03 22:37:25 +0000

    ahahaha

---
+++ Classwork/October 3, 2024.py
@@ -0,0 +1,6 @@
+#1.1
+city_one = input("Enter First City: ")
+city_two = input("Enter Second City: ")
+city_three = input("Enter Third City: ")
+list = [city_one, city_two, city_three]
+print(sorted(list))
