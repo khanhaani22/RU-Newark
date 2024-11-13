f403bc5c2f32de1c7a8f4b24c430b05e75ef2b57
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2024-11-13 23:49:51 +0000

    yes

---
+++ Assignment5/AreaAndPerimeter2.py
@@ -0,0 +1,23 @@
+import Assignment5/geometry.py
+"""
+NAME: Haani Khan
+DATE: November 11, 2024
+UNIT: 5
+ASSIGNMENT:
+"""
+
+# code starts here
+    print("Would you like to calculate... ")
+    print("1. Area of a circle")
+    print("2. Circumference of a circle")
+    print("3. Area of a rectangle")
+    print("4. Perimeter of a rectangle")
+    print("q. Quit the program)
+    inp = input("Type the option (1,2,3,4, or q): ")
+    if inp = 1:
+        areaCircle(radius)
+# OUTPUT: 
+"""
+
+"""
+
