5b293854e1f1ac2accee352ebe4b6d6fb7f47d53
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2024-11-14 00:42:29 +0000

    finished

--- Assignment5/AreaAndPerimeter2.py
+++ Assignment5/AreaAndPerimeter2.py
@@ -1,4 +1,4 @@
-
+import geometry
 """
 NAME: Haani Khan
 DATE: November 11, 2024
@@ -7,20 +7,31 @@ ASSIGNMENT:
 """
 
 # code starts here
-print("Would you like to calculate... ")
-print("1. Area of a circle")
-print("2. Circumference of a circle")
-print("3. Area of a rectangle")
-print("4. Perimeter of a rectangle")
-print("q. Quit the program")
-inp = input("Type the option (1,2,3,4, or q): ")
-if inp == 1:
-    radius = input("What is the radius of the circle? \n")
-    areaCircle(radius)
-if inp == 2:
-    radius = input("What is the radius of the circle? \n")
-    circumCircle(radius)
-        
+i = 0
+while i == 0:
+    print("Would you like to calculate... ")
+    print("1. Area of a circle")
+    print("2. Circumference of a circle")
+    print("3. Area of a rectangle")
+    print("4. Perimeter of a rectangle")
+    print("q. Quit the program")
+    ans = (input("Type the option (1,2,3,4, or q): \n"))
+    if (ans == "1"):
+        radius = int(input("What is the radius of the circle? \n"))
+        geometry.areaCircle(radius)
+    if (ans == adius"2"):
+        radius = int(input("What is the radius of the circle? \n"))
+        geometry.circumCircle(radius)
+    if (ans == "3"):
+        length = int(input("What is the length of the rectangle? \n"))
+        width = int(input("What is the width of the rectangle? \n"))
+        geometry.areaRect(length, width)
+    if (ans == "4"): 
+        length = int(input("What is the length of the rectangle? \n"))
+        width = int(input("What is the width of the rectangle? \n"))
+        geometry.periRect(length, width)
+    if (ans == "q"):
+        i += 1
 # OUTPUT: 
 """
 
---
+++ Assignment5/geometry.py
@@ -0,0 +1,17 @@
+import math
+
+def areaCircle(radius):
+    area = math.pi e* radius ** 2
+    print(f"The area is {area} units squared.")
+
+def circumCircle(radius):
+    circumference = 2 * math.pi * radius
+    print(f"The circumference is {circumference} units.")
+
+def areaRect(length, width):
+    area = length * width
+    print(f"The area is {area} units squared.")
+
+def periRect(length, width):
+    perimeter = (2 * length) + (2 * width)
+    print(f"The perimeter is {perimeter} units.")
